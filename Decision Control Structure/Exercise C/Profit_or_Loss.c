5a5cedc55076f28bc89483a54233e23837f33320
Richa <richamsroy@gmail.com>
2026-09-04 09:36:05 +0530

    Fix nested repository folder

---
+++ Decision Control Structure/Exercise C/Profit_or_Loss.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    float costprice,sellingprice,profit,loss;
+    printf("Enter Cost Price of item: ");
+    scanf("%f",&costprice);
+    printf("Enter Selling Price of item: ");
+    scanf("%f",&sellingprice);
+    if(sellingprice>costprice)
+    {
+        profit=sellingprice-costprice;
+        printf("Seller has made profit\nProfit Made:%.2f",profit);
+    }
+    else if(costprice>sellingprice)
+    {
+        loss=costprice-sellingprice;
+        printf("Seller has incurred loss\nLoss Incurred:%.2f",loss);
+    }
+    else
+    {
+        printf("Seller has made neither profit nor incurred loss");
+    }
+}
